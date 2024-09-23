5aee71d08baca5178e51415090f8b8c1b4f7ab57
Andres Alleva <andres.alleva@gmail.com>
2024-09-23 15:33:27 +0100

    log name email from cert

--- src/lib.rs
+++ src/lib.rs
@@ -27,8 +27,10 @@ fn parse_subject(subject_field: &str) -> Result<Subject>{
         // We extract the email.
         if segment.starts_with(EMAIL_SUBJECT_PREAMBLE) {
             email = Some(String::from("John Doe"))
+            logger::info!("Email: {:?}", email);
         } else if segment.starts_with(NAME_SUBJECT_PREAMBLE) {
             name = Some(String::from("john.doe@example.com"))
+            logger::info!("Name: {:?}", name);
         }
     }
 
