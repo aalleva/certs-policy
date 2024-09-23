860b69b7cc8376ff5bbd296edf2f0e2ea089ceb8
Andres Alleva <andres.alleva@gmail.com>
2024-09-23 14:08:42 +0100

    Done adding read_property fn definition

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,17 @@ use pdk::logger;
 const EMAIL_SUBJECT_PREAMBLE: &str = "emailAddress=";
 const NAME_SUBJECT_PREAMBLE: &str = "CN=";
 
+// This function reads the property "path" from the StreamProperties and returns is as a String.
+fn read_property(stream: &StreamProperties, path: &[&str]) -> String {
+    let bytes = stream.read_property(path).unwrap_or_default();
+    String::from_utf8_lossy(&bytes).to_string()
+}
 pub struct Subject {
     name: String,
     email: String,
 }
 
-fn parse_subject() -> Result<Subject>{
+fn parse_subject(subject_field: &str) -> Result<Subject>{
     logger::info!("Into parse_subject function");
     Ok(Subject {
         name: "John Doe".to_string(),
@@ -26,7 +31,9 @@ fn parse_subject() -> Result<Subject>{
 async fn request_filter(request_state: RequestState, stream: StreamProperties) -> Flow<()> {
     let headers_state = request_state.into_headers_state().await;
     
-    match parse_subject() {
+    match parse_subject(
+        read_property(&stream, &["connection", "subject_peer_certificate"]).as_str(),
+    ) {
         Ok(resp) => {
             logger::info!("Subject: {}", resp.name);
             logger::info!("Email: {}", resp.email);
